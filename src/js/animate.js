8a687ce28fdef10228f65fa9a780d224144651af
Torbjörn Lundberg <torbjorn@basstrombone.info>
2025-02-21 13:00:54 +0100

    lagt till jsdoc

--- src/js/animate.js
+++ src/js/animate.js
@@ -1,6 +1,10 @@
 "use strict";
 
-// script.js
+
+/**
+ * Eventlyssnare för knappen start som sköter animeringen
+ * @event
+ */
 document.getElementById('animatestart').addEventListener('click', function() {
     const element = document.getElementById('animatedElement');
     element.classList.toggle('animate');
